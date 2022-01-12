78b9837032607bbb09b17a08690bc37db0009ed1
raitztiago1 <raitztiago1@gmail.com>
2022-01-12 16:02:21 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,34 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-    return(
-        <Pagination/>
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-2 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 export default Listing;
